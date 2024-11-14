f2a6391a23c05f42ddd1c19721868da7de3aed73
jeff-0324 <jeffap324@gmail.com>
2024-11-14 21:32:00 +0900

    [feat] : Lv.1 구현

--- calculator_personal/calculator_personal/ViewController.swift
+++ calculator_personal/calculator_personal/ViewController.swift
@@ -3,12 +3,37 @@ import UIKit
 import SnapKit
 
 class ViewController: UIViewController {
+    
+    let label = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        configureUI()
     }
 
- 
+    func configureUI() {
+        
+        view.backgroundColor = .black
+        
+        label.text = "12345"
+        label.textColor = .white
+        label.backgroundColor = .black
+        label.textAlignment = .right
+        label.font = .boldSystemFont(ofSize: 60)
+        
+        [label]
+            .forEach { view.addSubview($0) }
+        
+        label.snp.makeConstraints {
+            $0.height.equalTo(100)
+            $0.leading.equalToSuperview().offset(30)
+            $0.trailing.equalToSuperview().offset(-30)
+            $0.top.equalToSuperview().offset(200)
+        }
+    }
 }
 
+#Preview {
+    ViewController()
+}
